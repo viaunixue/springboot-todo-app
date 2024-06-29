d246c6d030a955ff2439d318f0cd82b3f2d197fc
JongHyunJung <jjh19960115@gmail.com>
2024-06-29 21:17:41 +0900

    feat: Add 5 API Version Management Way

---
+++ src/main/java/com/chzzk/study/learn_spring_boot/versioning/PersonV2.java
@@ -0,0 +1,22 @@
+package com.chzzk.study.learn_spring_boot.versioning;
+
+public class PersonV2 {
+    private Name name;
+
+    public PersonV2(Name name) {
+        this.name = name;
+    }
+
+    public Name getName() {
+        return name;
+    }
+
+    public void setName(Name name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "PersonV2 [name=" + name + "]";
+    }
+}
