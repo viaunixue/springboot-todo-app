5ee01386398bc9a84ac1637e117a78659dacc717
JongHyunJung <jjh19960115@gmail.com>
2024-06-30 16:14:25 +0900

    feat: Create API Filtering & Actuator & HAL Explorer

--- src/main/java/com/chzzk/study/learn_spring_boot/user/UserResource.java
+++ src/main/java/com/chzzk/study/learn_spring_boot/user/UserResource.java
@@ -1,6 +1,10 @@
 package com.chzzk.study.learn_spring_boot.user;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
+
 import jakarta.validation.Valid;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -24,15 +28,22 @@ public class UserResource {
     }
 
     // GET /users/{id}
+
+    // EntityModel
+    // WebMvcLinkBuilder
     @GetMapping("/users/{id}")
-    public User retrieveUser(@PathVariable int id) {
+    public EntityModel<User> retrieveUser(@PathVariable int id) {
         User user = service.findOne(id);
 
         if (user == null) {
             throw new UserNotFoundException("id:"+ id);
         }
 
-        return user;
+        EntityModel<User> entityModel = EntityModel.of(user);
+        WebMvcLinkBuilder link =linkTo(methodOn(this.getClass()).retrieveAllUsers());
+        entityModel.add(link.withRel("all-users"));
+
+        return entityModel;
     }
 
     // DELETE /users/{id}
