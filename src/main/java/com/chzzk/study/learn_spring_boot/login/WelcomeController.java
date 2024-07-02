97cf5d56a0217815e77eacd9402d5e0b3dcc1e04
JongHyunJung <jjh19960115@gmail.com>
2024-07-02 22:37:39 +0900

    feat: Remove Spring Security for Study

--- src/main/java/com/chzzk/study/learn_spring_boot/login/WelcomeController.java
+++ src/main/java/com/chzzk/study/learn_spring_boot/login/WelcomeController.java
@@ -1,69 +1,69 @@
-package com.chzzk.study.learn_spring_boot.login;
-
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.SessionAttributes;
-
-@Controller
-@SessionAttributes("name")
-public class WelcomeController {
-
-//    private Logger logger = LoggerFactory.getLogger(getClass());
+//package com.chzzk.study.learn_spring_boot.login;
 //
-//    private AuthenticationService authenticationService;
+//import org.springframework.security.core.Authentication;
+//import org.springframework.security.core.context.SecurityContextHolder;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.ui.ModelMap;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestMethod;
+//import org.springframework.web.bind.annotation.SessionAttributes;
 //
-//    public LoginController(AuthenticationService authenticationService){
-//        super();
-//        this.authenticationService = authenticationService;
+//@Controller
+//@SessionAttributes("name")
+//public class WelcomeController {
+//
+////    private Logger logger = LoggerFactory.getLogger(getClass());
+////
+////    private AuthenticationService authenticationService;
+////
+////    public LoginController(AuthenticationService authenticationService){
+////        super();
+////        this.authenticationService = authenticationService;
+////    }
+//
+////    @RequestMapping("login-test")
+////    public String gotoLoginTestPage(@RequestParam String name, ModelMap model) {
+////        model.put("name", name);
+////
+////        logger.debug("Request param is {} by logger", name);
+////        logger.info("Request param is {} by logger", name);
+////
+////        // NOT RECOMMENDED FOR PROD CODE
+////        System.out.println("Request param is " + name);
+////
+////        // 이렇게 하면 @ResponseBody Spring MVC -> 여기 있는 것을 바로 리턴
+////        return "loginTest";
+////    }
+//
+//    // login
+//    // GET, POST 모두 기능하고 있는데 어떻게 이게 GET 요청만 처리하도록 할 수 있을까?
+//    // 요청 메서드를 추가 파라미터로 넣어주면 됩니다.
+//    // gotoLoginPage -> gotoWelcomePage 수정
+//    @RequestMapping(value = "/", method = RequestMethod.GET)
+//    public String gotoWelcomePage(ModelMap model) {
+//        model.put("name", getLoggedinUsername());
+//        return "welcome";
 //    }
-
-//    @RequestMapping("login-test")
-//    public String gotoLoginTestPage(@RequestParam String name, ModelMap model) {
-//        model.put("name", name);
 //
-//        logger.debug("Request param is {} by logger", name);
-//        logger.info("Request param is {} by logger", name);
+//    private String getLoggedinUsername() {
+//        Authentication authentication =
+//            SecurityContextHolder.getContext().getAuthentication();
 //
-//        // NOT RECOMMENDED FOR PROD CODE
-//        System.out.println("Request param is " + name);
-//
-//        // 이렇게 하면 @ResponseBody Spring MVC -> 여기 있는 것을 바로 리턴
-//        return "loginTest";
+//        return authentication.getName();
 //    }
-
-    // login
-    // GET, POST 모두 기능하고 있는데 어떻게 이게 GET 요청만 처리하도록 할 수 있을까?
-    // 요청 메서드를 추가 파라미터로 넣어주면 됩니다.
-    // gotoLoginPage -> gotoWelcomePage 수정
-    @RequestMapping(value = "/", method = RequestMethod.GET)
-    public String gotoWelcomePage(ModelMap model) {
-        model.put("name", getLoggedinUsername());
-        return "welcome";
-    }
-
-    private String getLoggedinUsername() {
-        Authentication authentication =
-            SecurityContextHolder.getContext().getAuthentication();
-
-        return authentication.getName();
-    }
-
-//    @RequestMapping(value = "login", method = RequestMethod.POST)
-//    public String gotoWelcomePage(@RequestParam String name, @RequestParam String password, ModelMap model) {
 //
-//        if (authenticationService.authenticate(name, password)) {
-//            model.put("name", name);
-//            // model.put("password", password);
-//
-//            return "welcome";
-//        }
-//
-//        model.put("ErrorMessage", "Invalid Credentials! Please try again");
-//
-//        return "login";
-//    }
-}
+////    @RequestMapping(value = "login", method = RequestMethod.POST)
+////    public String gotoWelcomePage(@RequestParam String name, @RequestParam String password, ModelMap model) {
+////
+////        if (authenticationService.authenticate(name, password)) {
+////            model.put("name", name);
+////            // model.put("password", password);
+////
+////            return "welcome";
+////        }
+////
+////        model.put("ErrorMessage", "Invalid Credentials! Please try again");
+////
+////        return "login";
+////    }
+//}
--- src/main/java/com/chzzk/study/learn_spring_boot/todo/TodoControllerJpa.java
+++ src/main/java/com/chzzk/study/learn_spring_boot/todo/TodoControllerJpa.java
@@ -1,98 +1,98 @@
-package com.chzzk.study.learn_spring_boot.todo;
-
-import jakarta.validation.Valid;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.SessionAttributes;
-
-import java.time.LocalDate;
-import java.util.List;
-
-@Controller
-@SessionAttributes("name")
-public class TodoControllerJpa {
-
-    private TodoRepository todoRepository;
-
-    public TodoControllerJpa(TodoRepository todoRepository) {
-        super();
-        this.todoRepository = todoRepository;
-    }
-
-    @RequestMapping("list-todos")
-    public String listAllTodos(ModelMap model) {
-        String username = getLoggedinUsername(model);
-
-        List<Todo> todos = todoRepository.findByUsername(username);
-        model.addAttribute("todos", todos);
-
-        return "listTodos";
-    }
-
-    @RequestMapping(value = "add-todo", method = RequestMethod.GET)
-    public String showNewTodoPage(ModelMap model) {
-        String username = getLoggedinUsername(model);
-
-        // 단방향 바인딩 -> Controller에서 양식에 표시되는 값으로 바인딩되는 것
-        Todo todo = new Todo(0, username, "", LocalDate.now().plusYears(1), false);
-        model.put("todo", todo);
-        return "todo";
-    }
-
-    @RequestMapping(value = "add-todo", method = RequestMethod.POST)
-    public String showNewTodo(ModelMap model, @Valid Todo todo, BindingResult result) {
-        if (result.hasErrors()) {
-            return "todo";
-        }
-
-        String username = getLoggedinUsername(model);
-        todo.setUsername(username);
-        todoRepository.save(todo);
-        // todoService.addTodo(username, todo.getDescription(), todo.getTargetDate(), false);
-        return "redirect:list-todos";
-    }
-
-    @RequestMapping(value = "update-todo", method = RequestMethod.GET)
-    public String showUpdateTodoPage(@RequestParam int id, ModelMap model) {
-        // Todo todo = todoService.findById(id);
-        Todo todo = todoRepository.findById(id).get();
-        model.addAttribute("todo", todo);
-        return "todo";
-    }
-
-    @RequestMapping(value = "update-todo", method = RequestMethod.POST)
-    public String updateTodo(ModelMap model, @Valid Todo todo, BindingResult result) {
-
-        if (result.hasErrors()) {
-            return "todo";
-        }
-
-        String username = (String) model.get("name");
-        todo.setUsername(username);
-        // todoService.updateTodo(todo);
-        todoRepository.save(todo);
-        return "redirect:list-todos";
-    }
-
-    @RequestMapping("delete-todo")
-    public String deleteTodo(@RequestParam int id) {
-        // todoService.deleteById(id);
-        todoRepository.deleteById(id);
-        return "redirect:list-todos";
-    }
-
-    private String getLoggedinUsername(ModelMap model) {
-        Authentication authentication =
-                SecurityContextHolder.getContext().getAuthentication();
-
-        return authentication.getName();
-    }
-
-}
-
+//package com.chzzk.study.learn_spring_boot.todo;
+//
+//import jakarta.validation.Valid;
+//import org.springframework.security.core.Authentication;
+//import org.springframework.security.core.context.SecurityContextHolder;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.ui.ModelMap;
+//import org.springframework.validation.BindingResult;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestMethod;
+//import org.springframework.web.bind.annotation.RequestParam;
+//import org.springframework.web.bind.annotation.SessionAttributes;
+//
+//import java.time.LocalDate;
+//import java.util.List;
+//
+//@Controller
+//@SessionAttributes("name")
+//public class TodoControllerJpa {
+//
+//    private TodoRepository todoRepository;
+//
+//    public TodoControllerJpa(TodoRepository todoRepository) {
+//        super();
+//        this.todoRepository = todoRepository;
+//    }
+//
+//    @RequestMapping("list-todos")
+//    public String listAllTodos(ModelMap model) {
+//        String username = getLoggedinUsername(model);
+//
+//        List<Todo> todos = todoRepository.findByUsername(username);
+//        model.addAttribute("todos", todos);
+//
+//        return "listTodos";
+//    }
+//
+//    @RequestMapping(value = "add-todo", method = RequestMethod.GET)
+//    public String showNewTodoPage(ModelMap model) {
+//        String username = getLoggedinUsername(model);
+//
+//        // 단방향 바인딩 -> Controller에서 양식에 표시되는 값으로 바인딩되는 것
+//        Todo todo = new Todo(0, username, "", LocalDate.now().plusYears(1), false);
+//        model.put("todo", todo);
+//        return "todo";
+//    }
+//
+//    @RequestMapping(value = "add-todo", method = RequestMethod.POST)
+//    public String showNewTodo(ModelMap model, @Valid Todo todo, BindingResult result) {
+//        if (result.hasErrors()) {
+//            return "todo";
+//        }
+//
+//        String username = getLoggedinUsername(model);
+//        todo.setUsername(username);
+//        todoRepository.save(todo);
+//        // todoService.addTodo(username, todo.getDescription(), todo.getTargetDate(), false);
+//        return "redirect:list-todos";
+//    }
+//
+//    @RequestMapping(value = "update-todo", method = RequestMethod.GET)
+//    public String showUpdateTodoPage(@RequestParam int id, ModelMap model) {
+//        // Todo todo = todoService.findById(id);
+//        Todo todo = todoRepository.findById(id).get();
+//        model.addAttribute("todo", todo);
+//        return "todo";
+//    }
+//
+//    @RequestMapping(value = "update-todo", method = RequestMethod.POST)
+//    public String updateTodo(ModelMap model, @Valid Todo todo, BindingResult result) {
+//
+//        if (result.hasErrors()) {
+//            return "todo";
+//        }
+//
+//        String username = (String) model.get("name");
+//        todo.setUsername(username);
+//        // todoService.updateTodo(todo);
+//        todoRepository.save(todo);
+//        return "redirect:list-todos";
+//    }
+//
+//    @RequestMapping("delete-todo")
+//    public String deleteTodo(@RequestParam int id) {
+//        // todoService.deleteById(id);
+//        todoRepository.deleteById(id);
+//        return "redirect:list-todos";
+//    }
+//
+//    private String getLoggedinUsername(ModelMap model) {
+//        Authentication authentication =
+//                SecurityContextHolder.getContext().getAuthentication();
+//
+//        return authentication.getName();
+//    }
+//
+//}
+//
