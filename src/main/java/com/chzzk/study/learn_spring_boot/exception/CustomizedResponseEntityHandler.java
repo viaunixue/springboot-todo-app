91ea690c164abcc5e5c5afbe25a5ea235d9707c8
JongHyunJung <jjh19960115@gmail.com>
2024-07-05 00:01:48 +0900

    feat: Update Generic Func

--- src/main/java/com/chzzk/study/learn_spring_boot/exception/CustomizedResponseEntityHandler.java
+++ src/main/java/com/chzzk/study/learn_spring_boot/exception/CustomizedResponseEntityHandler.java
@@ -24,7 +24,7 @@ public class CustomizedResponseEntityHandler extends ResponseEntityExceptionHand
                 ex.getMessage(),
                 request.getDescription(false));
 
-        return new ResponseEntity<ErrorDetails>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
+        return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
     @ExceptionHandler(UserNotFoundException.class)
@@ -33,7 +33,7 @@ public class CustomizedResponseEntityHandler extends ResponseEntityExceptionHand
                 ex.getMessage(),
                 request.getDescription(false));
 
-        return new ResponseEntity<ErrorDetails>(errorDetails, HttpStatus.NOT_FOUND);
+        return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
     }
 
     @Override
