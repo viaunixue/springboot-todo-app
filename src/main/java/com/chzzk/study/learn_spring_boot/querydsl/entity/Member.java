2e8c47b017981932dab29934ddc01d091c681fc8
JongHyunJung <jjh19960115@gmail.com>
2024-07-15 19:14:25 +0900

    feat: Initial Querydsl Setting

---
+++ src/main/java/com/chzzk/study/learn_spring_boot/querydsl/entity/Member.java
@@ -0,0 +1,42 @@
+package com.chzzk.study.learn_spring_boot.querydsl.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+// @Setter 실무에서 안쓰는 것 추천
+@Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(of = {"id", "username", "age"})
+public class Member {
+    @Id @GeneratedValue
+    @Column(name = "member_id")
+    private Long id;
+    private String username;
+    private int age;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "team_id")
+    private Team team;
+
+    public Member(String username, int age) {
+        this(username, age, null);
+    }
+
+    public Member(String username) {
+        this(username, 0);
+    }
+
+    public Member(String username, int age, Team team) {
+        this.username = username;
+        this.age = age;
+        if (team != null) {
+            changeTeam(team);
+        }
+    }
+
+    public void changeTeam(Team team) {
+        this.team = team;
+        team.getMembers().add(this);
+    }
+}
---
+++ src/test/java/com/chzzk/study/learn_spring_boot/querydsl/entity/MemberTest.java
@@ -0,0 +1,52 @@
+package com.chzzk.study.learn_spring_boot.querydsl.entity;
+
+import jakarta.persistence.EntityManager;
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
+
+import java.util.List;
+
+@SpringBootTest
+@Transactional
+@Commit
+class MemberTest {
+    @Autowired
+    EntityManager em;
+
+    @Test
+    public void testEntity() {
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        em.persist(teamA);
+        em.persist(teamB);
+
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamA);
+
+        Member member3 = new Member("member3", 30, teamB);
+        Member member4 = new Member("member4", 40, teamB);
+
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+
+        // 초기화
+        // flush 해서 영속성 컨텍스트에 있는 객체를 실제 쿼리를 만들어 DB에 날림
+        // clean 통해 영속성 컨텍스트를 완전히 초기화해서 캐시가 다 날아감
+        em.flush();
+        em.clear();
+
+        // 확인!
+        List<Member> members = em.createQuery("select m from Member m", Member.class)
+                .getResultList();
+
+        for (Member member : members) {
+            System.out.println("member = " + member);
+            System.out.println("-> member.team = " + member.getTeam());
+        }
+    }
+}
